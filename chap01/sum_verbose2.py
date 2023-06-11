57370e38824c6c7e68ff6080e52d0a60d796a27a
eyeuwoobi <eyeuwoobi0216@gmail.com>
2023-06-11 11:20:50 +0000

    6.11

--- chap01/sum_verbose2.py
+++ chap01/sum_verbose2.py
@@ -1,4 +1,4 @@
-# a부터 b까지 정수의 합 구하기 1
+# a부터 b까지 정수의 합 구하기 2
 
 print('a부터 b까지 정수의 합을 구합니다.')
 a = int(input('정수 a를 입력하세요.: '))
@@ -8,9 +8,8 @@ if a > b:
     a, b = b, a
 
 sum = 0
-for i in range(a, b+1):
+for i in range(a, b):
+    print(f'{i} + ', end='')
     sum += i
-    if i < b:
-        print(f'{i} + ', end='')
-    else:
-        print(f'{i} = {sum}')
+
+print(f'{b} = {sum + b}')
