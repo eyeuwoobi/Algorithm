25596f72a4bba398274da881c2e62e73ac1aa060
Yang <eyeuwoobi0216@gmail.com>
2024-07-23 16:33:48 +0900

    7.23

--- BOJ_class2/FizzBuzz.py
+++ BOJ_class2/FizzBuzz.py
@@ -1,2 +1,22 @@
 import sys
 
+
+counter = 1
+while True:
+    try:
+        i = int(sys.stdin.readline())
+        break
+    except ValueError:
+        counter += 1
+
+i += (4 - counter)
+
+if i % 3 == 0:
+    if i % 5 == 0:
+        print('FizzBuzz')
+    else:
+        print('Fizz')
+elif i % 3 != 0 and i % 5 == 0:
+    print('Buzz')
+else:
+    print(i)
---
+++ BOJ_class2/단어 정렬.py
@@ -0,0 +1,32 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+words = []
+words.append(sys.stdin.readline())
+
+while True:
+    try:
+        letter = sys.stdin.readline()
+        words.index(letter) 
+        if len(words[-1]) < len(letter):
+            words.append(letter)
+        elif len(words[0]) > len(letter):
+            words.insert(0, letter)
+        else:
+            head = 0
+            tail = 0
+            for i in range(len(words)):
+                if len(words[i]) == len(letter):
+                    head = i
+                    break
+            for i in range(len(words), -1, -1):
+                if len(words[i]) == len(letter):
+                    tail = i
+                    break
+            splitted_list = words[head:tail+1]
+        splitted_list.append(letter)
+        splitted_list.sort()
+        words  = words[:head] + splitted_list + words[tail:]
+    except ValueError:
+        pass
