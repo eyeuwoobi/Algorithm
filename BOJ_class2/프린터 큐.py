3442490e4109642b697bf7538066bf9ab9610eee
Yang <eyeuwoobi0216@gmail.com>
2024-08-01 20:02:36 +0900

    8.1

--- BOJ_class2/프린터 큐.py
+++ BOJ_class2/프린터 큐.py
@@ -5,14 +5,23 @@ t = int(sys.stdin.readline())
 
 for _ in range(t):
     n, m = map(int, sys.stdin.readline().split())
-    documents = deque(list(map(int, sys.stdin.readline().split())))
+    documents = list(map(int, sys.stdin.readline().split()))
     target = documents[m]
-    tagger = deque([0] * n)
+    tagger = [0] * n
     tagger[m] = 1
     counter = 0
     while True:
         if max(documents) > documents[0]:
             documents.append(documents[0])
-            documents = documents[1:]
-            
+            documents[0:1] = []
+            tagger.append(tagger[0])
+            tagger[0:1] = []
+        else:
+            counter += 1
+            if tagger[0] == 1:
+                print(counter)
+                break
+            else:
+                documents.pop(0)
+                tagger.pop(0)
     
