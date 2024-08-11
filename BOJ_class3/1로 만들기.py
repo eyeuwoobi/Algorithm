fc267e4d7a15030d9314854e621d5ea4863630c7
Yang <eyeuwoobi0216@gmail.com>
2024-08-11 16:13:01 +0900

    24.08.11

--- BOJ_class3/1로 만들기.py
+++ BOJ_class3/1로 만들기.py
@@ -4,40 +4,4 @@ from copy import copy
 input = sys.stdin.readline
 
 n = int(input())
-m = copy(n)
-counter_f = 0
-counter_s = 0
 
-while n > 1:
-    if n % 3 == 0:
-        counter_f += 1
-        n //= 3
-    elif n % 3 == 1:
-        counter_f += 1
-        n -= 1
-    elif n % 2 == 0:
-        counter_f += 1
-        n //= 2
-    else:
-        n -= 1
-        counter_f += 1
-
-while m > 1:
-    if m % 3 == 0:
-        counter_s += 1
-        m //= 3
-    elif m % 2 == 0:
-        counter_s += 1
-        m //= 2
-    elif m % 3 == 1:
-        counter_s += 1
-        m -= 1
-    else:
-        m -= 1
-        counter_s += 1
-    print(m)
-
-print(min(counter_f, counter_s))
-
-# 짝수인데 mod 3 = 1인 경우가 문제임.
-# 그럴 때마다 분기점 나눠서 계산하는게 필요한듯.
--- BOJ_class3/나는야 포켓몬 마스터 이다솜.py
+++ BOJ_class3/나는야 포켓몬 마스터 이다솜.py
@@ -3,25 +3,19 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-d = dict()
-ps = []
+name_dict = dict()
+num_dict = dict()
 
-# for i in range(n):
-#     name = input().rstrip()
-#     d[name] = str(i+1)
-    
-# # for _ in range(m):
-# #     quiz = input().rstrip()
-# #     if quiz in d:
-# #         print(d[quiz])
-# #     else:
-# #         for k, v in d.items():
-# #             if v == quiz:
-# #                 print(v)
-
-for _ in range(n):
-    ps.append(input().rstrip())
+for i in range(n):
+    name = input().rstrip()
+    name_dict[name] = i + 1
+    num_dict[i+1] = name
 
 for _ in range(m):
     quiz = input().rstrip()
+    try:
+        number = int(quiz)
+        print(num_dict[number])
+    except ValueError:
+        print(name_dict[quiz])
     
